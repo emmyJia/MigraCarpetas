a4d7074f6e32b84777693dc036a1bd14970b5169
Emmanuel Mejia <emejia@mbge.com.mx>
2018-06-15 10:42:24 -0500

    Subir proyecto

---
+++ MigraCarpetas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MigraCarpetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
